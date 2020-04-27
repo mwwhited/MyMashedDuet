842c37a52e9a13df9287df5af36d3238e44e7185
Matthew Whited <matt@whited.us>
2020-04-27 19:12:58 -0400

    update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,8 +2,8 @@
 
 cd /home/pi/MyMashedDuet
 git pull
-rsync /home/pi/MyMashedDuet/duet3/dsf/sd /opt/dsf -rv
-rsync /opt/dsf/sd /home/pi/MyMashedDuet/duet3/dsf -rv
+rsync /home/pi/MyMashedDuet/duet3/dsf/sd /opt/dsf -rv --exclude *.bin
+rsync /opt/dsf/sd /home/pi/MyMashedDuet/duet3/dsf -rv --exclude *.bin
 sudo chown pi:pi . -R
 git add -A duet3
 # git reset duet3/dsf_sd/www
