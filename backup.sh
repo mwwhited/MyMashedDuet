3453f7eb375d825358ed5efb321ae4a0c2e1c1fb
Matthew Whited <matt@whited.us>
2020-04-28 07:00:48 -0400

    Changes from DSF Detected: Tue 28 Apr 07:00:48 EDT 2020

--- backup.sh
+++ backup.sh
@@ -3,8 +3,9 @@
 cd /home/pi/MyMashedDuet
 git pull
 sudo chown pi /opt/dsf/sd -R
-#rsync /home/pi/MyMashedDuet/duet3/dsf/sd /opt/dsf -rv --exclude *.bin
-rsync /opt/dsf/sd /home/pi/MyMashedDuet/duet3/dsf -rv --exclude *.bin
+#sync back was causing issues and not really needed
+#rsync /home/pi/MyMashedDuet/duet3/dsf/sd /opt/dsf -rv --exclude *.bin 
+rsync /opt/dsf/sd /home/pi/MyMashedDuet/duet3/dsf -rv --exclude *.bin --exclude sd/gcodes/* --delete 
 sudo chown pi:pi . -R
 ./prettyjson.sh
 git add -A duet3
