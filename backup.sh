1edd91776c3dc86952a18bdb6e9055fbb2f8d447
Matthew Whited <matt@whited.us>
2020-04-27 21:01:27 -0400

    Changes from DSF Detected: Mon 27 Apr 21:01:27 EDT 2020

--- backup.sh
+++ backup.sh
@@ -2,6 +2,7 @@
 
 cd /home/pi/MyMashedDuet
 git pull
+sudo chown pi /opt/dsf/sd -R
 rsync /home/pi/MyMashedDuet/duet3/dsf/sd /opt/dsf -rv --exclude *.bin
 rsync /opt/dsf/sd /home/pi/MyMashedDuet/duet3/dsf -rv --exclude *.bin
 sudo chown pi:pi . -R
--- prettyjson.sh
+++ prettyjson.sh
@@ -1,6 +1,6 @@
 for file in duet3/dsf/sd/sys/*.json
 do 
-	echo make '$file' pretty
+	echo make $file pretty
 	export newFile=$(echo $file | sed 's/\(.*\.\)json/\1formatted.json/')
 	cat $file | python -m json.tool > $newFile
 	mv -f $newFile $file
