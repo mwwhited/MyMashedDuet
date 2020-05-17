b090d7d2a27b360a85091776c7309fee4e1f0b50
Matthew Whited <matt@whited.us>
2020-05-17 13:17:56 -0400

    Add mixer tool head script builder

---
+++ tools/RepRapFirmwareConfig/MixerHotendToolBuilder/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MixerHotendToolBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mixStates = 2;
+
+            var drivers = new[] { 0, 1, 2 };
+            int? heater = 1;
+            int? fan = 0;
+            var offsets = new { x = 0m, y = -19m, z = 0m, index = 0 };
+            var temperatures = new { active = 0, standby = 0 };
+
+            var template = new
+            {
+                input = "ToolTemplate.g",
+                target = "<<DefinitionsHere>>",
+                output = "Setup 3 Color"
+            };
+
+            // =MOD(INT((ROW()-2)/POWER(Mix_Steps,INDIRECT(ADDRESS(1,COLUMN())))),Mix_Steps)
+            // ROW()-2 = mix index
+            // INDIRECT(ADDRESS(1,COLUMN())) = extruder index
+
+            var extruderCount = drivers.Length;
+            var possibleMixes = (int)Math.Pow(mixStates, extruderCount);
+            var format = new
+            {
+                weight = new string('X', (int)Math.Ceiling(Math.Log(mixStates, 16))),
+                definitionSuffix = string.Join(' ', new[] {
+                    drivers.Any() ? $"D{string.Join(':', drivers)}":null,
+                    heater.HasValue ? $"H{heater}":null,
+                    fan.HasValue ? $"F{fan}":null,
+                }.Where(v => !string.IsNullOrWhiteSpace(v))),
+            };
+
+            var matrix = from mixIndex in Enumerable.Range(0, possibleMixes)
+                         let weights = (from extruder in Enumerable.Range(0, extruderCount)
+                                        select mixIndex / (int)Math.Pow(mixStates, extruder) % mixStates).ToArray()
+                         let totalWeight = weights.Sum()
+                         where totalWeight > 0
+                         let mixes = (from w in weights
+                                      select Math.Round((decimal)w / totalWeight, 2)).ToArray()
+                         let configuration = $"E{string.Join(":", mixes.Select(v => v.ToString("0.00")))}"
+                         let description = $"C{string.Join("", weights.Select(v => v.ToString(format.weight)))}"
+                         group new
+                         {
+                             totalWeight,
+                             Description = description,
+                             Configuration = configuration,
+                         } by configuration into possibleTools
+                         let distinctTool = possibleTools.First()
+                         orderby distinctTool.totalWeight, distinctTool.Description descending
+                         select distinctTool;
+
+            var tools = from t in matrix.Select((v, i) => new { Tool = v, ToolIndex = i + offsets.index })
+                        select new[]
+                        {
+                            $"; Define Tool {t.ToolIndex}",
+                            $@"M563 P{t.ToolIndex} S""{t.Tool.Description}"" {format.definitionSuffix}",
+                            $"G10 P{t.ToolIndex} X{offsets.x} Y{offsets.y} Z{offsets.z} R{temperatures.active} S{temperatures.standby}",
+                            $"M567 P{t.ToolIndex} {t.Tool.Configuration}",
+                            "",
+                        };
+            var replacement = tools.SelectMany(l => l)
+                                   .Aggregate(new StringBuilder(), (sb, l) => sb.AppendLine(l), sb => sb.ToString());
+
+            var body = File.ReadAllText(template.input)
+                           .Replace(template.target, replacement)
+                           ;
+
+            File.WriteAllText(template.output, body);
+        }
+    }
+}
---
+++ tools/RepRapFirmwareConfig/MixerHotendToolBuilder/ToolTemplate.g
@@ -0,0 +1,29 @@
+﻿M98 P"0:/macros/Config Scripts/Change Toolhead"
+M98 P"0:/macros/Config Scripts/Tool Setups/Parts Fan"
+
+M451						; FFF Mode
+
+;Heater 1 (HTF1)
+M308 S1 P"temp1" Y"thermistor" T100000 B4138 A"HTF1"	; configure sensor 1 as thermistor on pin temp1
+M950 H1 C"out1" T1			; create nozzle heater output on out1 and map it to sensor 1
+M143 H1 S280				; set temperature limit for heater 1 to 280C
+
+;Hotend Fan
+M950 F1 C"out4" Q50			; create fan 2 on pin out4 and set its frequency
+M106 P1 S0 H1 T30 C"Hotend"		; set fan 2 value. Thermostatic control is turned on
+
+;Tools
+<<DefinitionsHere>>
+M92 E96.2001				; Feed rate
+
+;Tuning result for heaters
+M307 H1 A424.6 C236.6 D6.6 S1.0 V24.0 B0 ; tune heater for reprap diamond
+
+;Set filament size 
+M200 D1.75
+
+echo "Configured for 3 to 1 Bowden Hotend"
+
+M28 "0:/sys/lasttool.g"
+M98 P"0:/macros/Config Scripts/Setup 3 to 1 Hotend"
+M29
